4139071598a2d797f9b085e426c12761b79c6b25
X-ljy <17637938901@163.com>
2019-09-15 17:15:51 +0800

    5015

--- src/main/java/com/ljy/iot/handler/MyHandler.java
+++ src/main/java/com/ljy/iot/handler/MyHandler.java
@@ -47,13 +47,13 @@ public class MyHandler extends SimpleChannelInboundHandler {
 
         if(byteBuf.getByte(DataSunConfig.data_type_index) == DataSunConfig.data_type){
             String[] strings = getData(byteBuf);
-            Entity5016 entity5016 = new Entity5016(strings[0],strings[1],strings[2],strings[3],strings[4],strings[5],strings[6],
+            Entity5015 entity5015 = new Entity5015(strings[0],strings[1],strings[2],strings[3],strings[4],strings[5],strings[6],
                     strings[7],strings[8],strings[9],strings[10],strings[11],strings[12],strings[13],strings[14],strings[15],
                     strings[16],strings[17],strings[18],strings[19],strings[20],strings[21],strings[22],strings[23],strings[24],
                     strings[25],strings[26],strings[27],strings[28],strings[29],strings[30],strings[31],strings[32],strings[33],
                     strings[34],strings[35],strings[36],strings[37],strings[38],strings[39],strings[40],strings[41],strings[42],
                     strings[43],strings[44],strings[45],strings[46],strings[47],strings[48],strings[49],strings[50],strings[51]);
-            TSDButil.doExecuteInsert(entity5016.toSqlString());
+            TSDButil.doExecuteInsert(entity5015.toSqlString());
         }
 
         //诱娥心跳数据
